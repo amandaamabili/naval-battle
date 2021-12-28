ea2a7c1a2688b82f2d56f46556bd6b1152b9b18c
Danilo Soares <danilo.mota.soares@gmail.com>
2021-12-28 14:08:06 -0300

    Added quick explanation to the use of the random Number Function;

--- src/src/br/com/letscode/navalbattle/Randomizer.java
+++ src/src/br/com/letscode/navalbattle/Randomizer.java
@@ -6,6 +6,10 @@ public class Randomizer {
 
     private static Random random = new Random();
 
+
+//    Para usar um número aleatório de 0 a 9 (índices possíveis das colunas e casas do tabuleiro),
+//    basta chamar a função abaixo onde se quer o número;
+//    Ex.: playerBoard[randomCoordinatesGenerator()][randomCoordinatesGenerator()];
     public static int randomCoordinatesGenerator() {
         return random.nextInt(10);
     }
